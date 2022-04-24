1328f85e21d58eeeb1873e67c00701785e27db34
jonatansabino <jonatan.wsabino@hotmail.com>
2022-04-24 09:38:39 -0300

    Atualização

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,52 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  
+  weight: number;
+  height: number;
+  
+  constructor(private toastController: ToastController) {}
 
-  constructor() {}
+  isFormValid() {
+    return !(this.weight && this.height && this.weight > 0 && this.height > 0);
+  }
 
+  ImcCalcular(IMC: number){
+    if (IMC < 18.5){
+      return 'Abaixo do Peso';
+    } else if (IMC < 25){
+      return 'Normal';
+    } else if (IMC < 30) {
+      return 'Sobrepeso';
+    } else if (IMC < 40){
+      return 'Odesidade';
+    } else {
+      return 'Odesidade Grave';
+    }
+  }
+
+  onCalcular(){
+    const IMC = this.weight / (this.height * this.height);
+    const degree = this.ImcCalcular(IMC);
+    this.showMessage(`IMC = ${IMC.toFixed(2)}   -   ${degree}`)
+  }
+
+  async showMessage(msg: string){
+    const previousToast = await this.toastController.getTop();
+    if (previousToast){
+      await this.toastController.dismiss();
+    }
+
+    const toast = await this.toastController.create({
+      message: msg,
+      color: 'tertiary',
+      buttons: [
+        {
+          icon: 'close'
+        }
+      ]
+    });
+
+    toast.present();
+  }
 }
